2ee9d40f6cf6279448d3dcfdcc4670d8397ca9ad
Andrii Bondarenko <lanbordik13@gmail.com>
2023-01-19 19:39:37 +0200

    minor change

--- Assets/Scripts/BlocksMergedSignal.cs
+++ Assets/Scripts/BlocksMergedSignal.cs
@@ -6,37 +6,6 @@ using Zenject;
 
 public class BlocksMergedSignal 
 {
-    static System.Random rnd = new System.Random();
-    [SerializeField] int numberToSpawn = 2;
-    Helper helper;
-
-    NumbersPool numbersPool;
-    public BlocksMergedSignal(Helper _helper, NumbersPool _numbersPool)
-    {
-        numbersPool = _numbersPool;
-        helper = _helper;
-    }
-    public void SayHello()
-    {
-        Debug.Log("HELLO FROM NEW SPAWNER AHAHA");
-    }
-    public void SpawnNumbers()
-    {
-        int amountOfSpawnedNumbers = UnityEngine.Random.Range(2, 4);
-        var dictValues = helper.NodesDictValues();
-        var freeSpots = dictValues.Where(s => s.NumberNode == null)
-                                .OrderBy(s => rnd.Next())
-                                .Take(amountOfSpawnedNumbers)
-                                .ToList();
-
-        foreach (var spot in freeSpots)
-        {
-            Debug.Log("BLOCKS MERGED SIGNAL IS NOW SPAWNING AHAHAHA");    
-            NumberNode pooledObject = numbersPool.GetFromPool();
-            pooledObject.transform.position = spot.transform.position;
-            pooledObject.InitNumberNode(helper.GetNumberByValue(numberToSpawn));
-            helper.SetNumberNode(spot.transform.position, pooledObject);
-        }
-
-    }
+    
+    
 }
